2c7e92dbb2eaf18a392abf3249ad9f6b94ef5edd
David-Kwech <david.kwechgomez@gmail.com>
2025-04-22 12:02:25 +0200

    subida examen solucion 2

--- Exámenes/EXA_EVA2_solucion/p05.js
+++ Exámenes/EXA_EVA2_solucion/p05.js
@@ -1,7 +1,11 @@
-/* 
-5. 
-*/
+/*
 
+5.- Dado el siguiente array:
+const aPersonas = ["Jose","Chema","Pepe","Bartolito","Begoña"];
+Escribe el código para intercambiar los valores de las posiciones 2 y 3, 
+independientemente de lo que contenga en esas posiciones.
+
+*/
 const aPersonas = ["Jose","Chema","Pepe","Bartololito",""];
 
 let aux = aPersonas[1];
