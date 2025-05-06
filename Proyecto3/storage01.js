7473e046067286ac0349763a11cdaf9b68da566e
David-Kwech <david.kwechgomez@gmail.com>
2025-05-06 11:17:01 +0200

    subida storage

---
+++ Proyecto3/storage01.js
@@ -0,0 +1,31 @@
+localStorage.setItem('nombre','Movil_de_Raul');
+localStorage.setItem('nombre2','Album_de_Brito');
+
+//alert(localStorage.getItem('nombre2'));
+
+
+let aFrutas = ['Kiwi','Movil_de_Raul','Fruta del dragón'];
+
+localStorage.setItem('arrayFrutas',aFrutas);
+
+localStorage.setItem('arrayFrutas',JSON.stringify(aFrutas));
+console.log(aFrutas);
+
+
+let otrasFrutas = JSON.parse(localStorage.getItem('arrayFrutas'));
+console.log(otrasFrutas);
+
+
+
+/*Hacemos un boton para guardar claves con valor*/
+let miBoton = document.getElementById('miBoton');
+
+function init() {
+    
+miBoton.addEventListener ( 'click', () => {
+    let miClave = document.getElementById('miClave').value;
+    let miValor = document.getElementById('miValor').value;
+
+    localStorage.setItem(miClave,miValor);
+});
+}
